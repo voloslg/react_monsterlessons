4c44b036916c8ec482b72cd11db825239fe106c4
volosgoto <volosgoto@yahoo.com>
2019-03-21 15:07:28 +0200

    display dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import RegistrationForm from "./RegistrationForm";
+import Dropdown from "./Dropdown";
 
 let menu = [
   { link: "/articles", label: "Articles" },
@@ -15,6 +16,7 @@ class App extends Component {
         <h1>Test from App</h1>
         <Header items={menu} />
         <RegistrationForm />
+        <Dropdown />
       </div>
     );
   }
